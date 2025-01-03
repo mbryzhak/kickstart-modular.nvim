866f514e109f7863a4889edcbf0e27278f27b586
Mykhailo Bryzhak <mbryzhak@gmail.com>
2025-01-03 15:17:20 +0100

    Initial modifications

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- [[ mbryzhak mods ]]
+vim.keymap.set('n', '<leader>pv', ':Ex<CR>', { desc = 'Return to the list view' })
+vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -177,9 +177,12 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        --clangd = {},
+        cpptools = {},
+        buildifier = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        -- jinja_lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
