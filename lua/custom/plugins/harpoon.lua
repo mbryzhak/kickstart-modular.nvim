d4412a6c761e5e86832992727cf358722c1abf7b
Mykhailo Bryzhak <mbryzhak@gmail.com>
2025-01-09 23:11:04 +0100

    Add harpoon delete file keymap

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -3,38 +3,124 @@ return {
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('harpoon'):setup()
+  end,
+  keys = function()
     local harpoon = require 'harpoon'
 
-    -- REQUIRED
-    harpoon:setup()
-    -- REQUIRED
+    local conf = require('telescope.config').values
 
-    vim.keymap.set('n', '<leader>a', function()
-      harpoon:list():add()
-    end)
-    vim.keymap.set('n', '<C-e>', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
 
-    vim.keymap.set('n', '<C-h>', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<C-t>', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<C-n>', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<C-s>', function()
-      harpoon:list():select(4)
-    end)
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
 
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<C-S-P>', function()
-      harpoon:list():prev()
-    end)
-    vim.keymap.set('n', '<C-S-N>', function()
-      harpoon:list():next()
-    end)
+      local make_finder = function()
+        local paths = {}
+
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require('telescope.finders').new_table {
+          results = paths,
+        }
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table {
+            results = file_paths,
+          },
+          previewer = false,
+          sorter = conf.generic_sorter {},
+          layout_strategy = 'center',
+          layout_config = {
+            preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+
+            width = function(_, max_columns, _)
+              return math.min(max_columns, 80)
+            end,
+
+            height = function(_, _, max_lines)
+              return math.min(max_lines, 15)
+            end,
+          },
+          borderchars = {
+            prompt = { '─', '│', ' ', '│', '╭', '╮', '│', '│' },
+            results = { '─', '│', '─', '│', '├', '┤', '╯', '╰' },
+            preview = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+          },
+          attach_mappings = function(prompt_buffer_number, map)
+            map('i', '<c-d>', function()
+              local state = require 'telescope.actions.state'
+              local selected_entry = state.get_selected_entry()
+              local current_picker = state.get_current_picker(prompt_buffer_number)
+
+              harpoon:list():remove(selected_entry)
+              current_picker:refresh(make_finder())
+            end)
+
+            return true
+          end,
+        })
+        :find()
+    end
+
+    return {
+      {
+        '<leader>ft',
+        function()
+          toggle_telescope(harpoon:list())
+        end,
+        desc = 'Harpoon [T]elescope View',
+      },
+      {
+        '<leader>fh',
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = '[H]arpoon Default View',
+      },
+      {
+        '<leader>fa',
+        function()
+          harpoon:list():add()
+        end,
+        desc = 'Harpoon [A]dd file',
+      },
+      {
+        '<leader>fd',
+        function()
+          local idx = vim.fn.line '.'
+          while idx < harpoon:list():length() do
+            local next_item = harpoon:list():get(idx + 1)
+            harpoon:list():replace_at(idx, next_item)
+            idx = idx + 1
+          end
+          harpoon:list():remove_at(idx)
+          harpoon.ui:close_menu()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = 'Harpoon [D]elete file',
+      },
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      {
+        '<C-S-P>',
+        function()
+          harpoon:list():prev()
+        end,
+      },
+      {
+        '<C-S-N>',
+        function()
+          harpoon:list():next()
+        end,
+      },
+    }
   end,
 }
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,7 +4,6 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
@@ -17,10 +16,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
